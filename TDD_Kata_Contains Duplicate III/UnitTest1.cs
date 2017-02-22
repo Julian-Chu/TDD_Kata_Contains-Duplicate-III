0d72c797211e5c790045026c314a71a40bd20540
Julian <yulang.chu@gmail.com>
2017-02-22 16:38:42 +0100

    add failed test case: nums{-1,2147483647},k=1,t=2147483647

--- TDD_Kata_Contains Duplicate III/UnitTest1.cs
+++ TDD_Kata_Contains Duplicate III/UnitTest1.cs
@@ -72,6 +72,32 @@ namespace TDD_Kata_Contains_Duplicate_III
             //Assert
             Assert.IsTrue(actual);
         }
+         
+        [TestMethod]
+        public void ContainsNearByAlmostDuplicate_Given_nums_0_2_4_2_kIs2_tIs0_Return_true()
+        {
+            //Assign
+            int[] nums = { 0, 2, 4, 2 };
+            int t = 0;
+            int k = 2;
+            //Act
+            bool actual = Solutions.ContainsNearbyAlmostDuplicate(nums, k, t);
+            //Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ContainsNearByAlmostDuplicate_Given_nums_minus1_2147483647_kIs1_tIs2147483647_Return_false()
+        {
+            //Assign
+            int[] nums = {-1,2147483647};
+            int k = 1;
+            int t = 2147483647;
+            //Act
+            bool actual = Solutions.ContainsNearbyAlmostDuplicate(nums, k, t);
+            //Assert
+            Assert.IsFalse(actual);
+        }
     }
 
     internal class Solutions
@@ -80,6 +106,7 @@ namespace TDD_Kata_Contains_Duplicate_III
         {
             if (k > 0)
             {
+                
                 for (int i = 0; i < nums.Length - 1; i++)
                 {
                     int searchLength = k > nums.Length - 1 ? nums.Length - 1 : k;

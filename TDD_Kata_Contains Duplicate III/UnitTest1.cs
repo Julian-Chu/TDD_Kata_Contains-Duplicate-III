b7854446fb3dfdfdd694fda375557def9aa7b1cf
USER <USER@MSI>
2017-02-22 13:41:49 +0100

    Add project files.

---
+++ TDD_Kata_Contains Duplicate III/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_Kata_Contains_Duplicate_III
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
